cfa90898777f8ab6db9a30ba292455a2ac524735
diana <udnkta@gmail.com>
2025-02-07 20:20:03 +0500

    initial commit

---
+++ src/main/java/com/practice/coffeemachinediana/CoffeemachineDianaApplication.java
@@ -0,0 +1,13 @@
+package com.practice.coffeemachinediana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeemachineDianaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeemachineDianaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/coffeemachinediana/CoffeemachineDianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.coffeemachinediana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeemachineDianaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
